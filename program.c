af680bc00620f71cdbc37565c5649e58ba9e687c
Dawid Walczak <walczakdawid321@gmail.com>
2025-01-24 10:16:28 +0100

    Korekta menu wyboru bryły

--- program.c
+++ program.c
@@ -10,7 +10,12 @@ int main()
 {
     float a, b, h,r;
     int wybor;
+    while (1)
+    {
+
+
     printf("Wybierz bryle ktora chcesz obliczyc\n");
+    printf("0. Wyjscie\n");
     printf("1. Objetosc prostopadloscianu\n");
     printf("2. Objetosc kuli\n");
     printf("Wybor:");
@@ -22,7 +27,10 @@ int main()
     scanf ("%f", &r);
 
     printf("\nObjetosc kuli: %f", ball_volume(r));
+    printf("\n");
     }
+
+
     else if (wybor==1)
     {
 
@@ -35,7 +43,13 @@ int main()
    printf("Podaj wysokosc podstawy prostopadloscianu\n");
    scanf("%f", &h);
 
-   printf("\nObjetosc prostopadloscianu: %f\n", cuboid_volume(a, b, h)); }
+   printf("\nObjetosc prostopadloscianu: %f\n", cuboid_volume(a, b, h));
+    printf("\n");
+     }
+else{
+    return 0;
 
+}
+}
     return 0;
 }
